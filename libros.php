573669fe054f04b878796216a2fc3b125d2579ad
robertofuentes866 <111166323+robertofuentes866@users.noreply.github.com>
2022-08-22 08:27:55 -0600

    echo en construct

--- libros.php
+++ libros.php
@@ -2,7 +2,7 @@
  class libros {
    public function __construct(protected int $paginas) {
       $this->paginas = $paginas;
-    
+      echo "haciendo pruebas";
    }
   
   public function getPaginas() {
